0544fc5078189e9e07a41ab75213fd6575862986
MudassirAS <mudassiras786@gmail.com>
2026-06-09 18:57:30 +0500

    posts by category

--- blog_main/urls.py
+++ blog_main/urls.py
@@ -19,9 +19,11 @@ from django.urls import path
 from blog_main import settings
 from . import views
 from django.conf.urls.static import static
+from django.urls import include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
+    path('category/', include('blogs.urls')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
