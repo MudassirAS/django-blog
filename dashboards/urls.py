d0f6ce9f8c0fcfc086fc5f735bbe2759fde18b3e
MudassirAS <mudassiras786@gmail.com>
2026-06-25 23:41:38 +0500

    Added the posts page in dashboard

--- dashboards/urls.py
+++ dashboards/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('categories/add/', views.add_category, name='add_category'),
     path('categories/edit/<int:pk>/', views.edit_category, name='edit_category'),
     path('categories/delete/<int:pk>/', views.delete_category, name='delete_category'),
+    # Blog/Post CRUD operations
+    path('posts/', views.posts, name='posts')
 ]
